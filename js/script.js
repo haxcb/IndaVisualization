2c2342a11583ff9dcb010464e13063c6638cdc9d
ssumal <ssumal@sfu.ca>
2014-07-31 18:34:14 -0700

    Basic Parse of Data

--- js/script.js
+++ js/script.js
@@ -13,4 +13,31 @@ var svg = d3.select(".container").append("svg")
 		
 var dataNodes = [],
 	dataLinks = [];
+
+
+d3.csv("http://www.sfu.ca/~ssumal/Inda/data/indaData.csv", function(csv) { 
+    return {
+    	"Name": csv.Name,
+      	"AKA": csv.AKA,
+        "FamilyName": csv.FamilyName,
+        "Importance": +csv.Importance,
+        "Gender": csv.Gender,
+        "ParentOf": d3.csv.parseRows(csv.ParentOf.replace(/\s*;\s*/g, ","))[0],
+        "SpouseOrBetrothed": csv.SpouseOrBetrothed,
+        "InloveWith": d3.csv.parseRows(csv.InloveWith.replace(/\s*;\s*/g, ","))[0],
+        "ChildOf": d3.csv.parseRows(csv.ChildOf.replace(/\s*;\s*/g, ","))[0],
+        "Siblings": d3.csv.parseRows(csv.Siblings.replace(/\s*;\s*/g, ","))[0],
+        "AcademyClassmates": d3.csv.parseRows(csv.AcademyClassmates.replace(/\s*;\s*/g, ","))[0],
+        "CousinsWith": d3.csv.parseRows(csv.CousinsWith.replace(/\s*;\s*/g, ","))[0],
+        "RunnerFor": csv.RunnerFor,
+        "PimRyala":  +csv.PimRyala,
+        "KodlMarines":  +csv.KodlMarines,
+        "AcademyTeacherFor":  d3.csv.parseRows(csv.AcademyTeacherFor.replace(/\s*;\s*/g, ","))[0],
+    };
+}, function(error, rows){
+	if(window.console){
+		window.console.log(rows);
+		console.log(JSON.stringify(error, null, "\t"));
+	}
+});
 	
